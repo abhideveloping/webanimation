829448c1520edfd0767ec0445ecd9805ab3b8fab
abhideveloping <abhinandantiwari150@gmail.com>
2024-08-08 22:17:05 +0530

    gsap navigation animation

---
+++ navanimation/home.js
@@ -0,0 +1,9 @@
+gsap.from('.navbar', {y:-50, duration:2});
+
+gsap.to('.box', {x:1000, scale:"1.4", rotation:360, duration:2});
+
+gsap.from('.nav-box1', {opacity:0.1, duration:8});
+
+gsap.from('.nav-box2', {opacity:0.1, duration:8});
+
+gsap.from('.list', {fill:"orange", ease: "bounce.out", duration:5});
